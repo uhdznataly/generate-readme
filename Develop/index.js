0a9be4e25982886dd95d5826809297731f70fb90
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2022-11-14 18:09:21 -0600

    delete and re add package.json file

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,36 @@ const fs= require("fs");
 const generateMarkdown= require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =  [{
+    type: 'input',
+    name: 'Title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'location',
+    message: 'Where are you from?',
+  },
+  {
+    type: 'input',
+    name: 'hobby',
+    message: 'What is your favorite hobby?',
+  },
+  {
+    type: 'input',
+    name: 'food',
+    message: 'What is your favorite food?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username',
+  },
+  {
+    type: 'input',
+    name: 'linkedin',
+    message: 'Enter your LinkedIn URL.',
+  },]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
